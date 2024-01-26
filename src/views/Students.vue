af7672454e5941219729e777da3329c19249bc7c
davidjaa <davidjaanson@gmail.com>
2024-01-26 14:54:00 +0200

    Task1

--- src/views/Students.vue
+++ src/views/Students.vue
@@ -4,6 +4,7 @@
 
 <div class="container">
     <table>
+      <thead>
           <tr>
             <th>St. Code</th>
             <th>Course name</th>
@@ -14,6 +15,20 @@
             <th>UT Course ECTS</th>
             <th>Decision</th>
           </tr>
+      </thead>
+      <tbody>
+          <tr v-for="request in rplrequests" :key="request.id">
+            <td>{{ request.studentcode }}</td>
+            <td>{{ request.coursename }}</td>
+            <td>{{ request.coursecode }}</td>
+            <td>{{ request.courseects }}</td>
+            <td>{{ request.utcoursename }}</td>
+            <td>{{ request.utcoursecode }}</td>
+            <td>{{ request.utcourseects }}</td>
+            <td>{{ request.decision }}</td>
+          </tr>
+        </tbody>
+
     </table>
     
   </div>
@@ -50,6 +65,7 @@ h3 {
   text-align: center;
   color: rgb(8, 110, 110);
 }
+
 .container {
   background: #d5d7d8;
   box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
@@ -59,5 +75,26 @@ h3 {
   width: 90%;
   display: flex;
   justify-content: center;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 15px;
+}
+
+th, td {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+td {
+  background-color: bisque;
+}
+
+th {
+  background-color: #4CAF50;
+  color: white;
 }
 </style>
